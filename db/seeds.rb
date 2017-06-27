939b1cd9f7cec3c7b98956d5a0c6190da0eb1fd4
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 16:26:00 +0000

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+  end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
     body: "M, O, A, I; this simulation is not as the former: and
 yet, to crush this a little, it would bow to me, for
 every one of these letters are in my name. Soft!
-here follows prose."
+here follows prose.",
+  topic_id: Topic.last.id
     )
 end
 
@@ -19,10 +28,10 @@ puts "10 blog posts created"
   
   puts "5 skills created"
   
-  9.times do |portfolio_item|
+  8.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}",
-      subtitle: "My great service",
+      subtitle: "Ruby on Rails",
       body: "If this fall into thy hand, revolve. In my stars I
 am above thee; but be not afraid of greatness: some
 are born great, some achieve greatness, and some
@@ -34,4 +43,20 @@ cast thy humble slough and appear fresh.",
       thumb_image: "http://via.placeholder.com/350x200",
       )
   end
+    1.times do |portfolio_item|
+    Portfolio.create!(
+      title: "Portfolio title: #{portfolio_item}",
+      subtitle: "Angular",
+      body: "If this fall into thy hand, revolve. In my stars I
+am above thee; but be not afraid of greatness: some
+are born great, some achieve greatness, and some
+have greatness thrust upon 'em. Thy Fates open
+their hands; let thy blood and spirit embrace them;
+and, to inure thyself to what thou art like to be,
+cast thy humble slough and appear fresh.",
+      main_image: "http://via.placeholder.com/600x400",
+      thumb_image: "http://via.placeholder.com/350x200",
+      )
+  end
+  
   puts "9 portfolios items created"
