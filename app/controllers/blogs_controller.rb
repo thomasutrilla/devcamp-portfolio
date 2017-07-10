02a0fa5dc618c9418359b43f2c5ccfc5936fbe0b
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:55:46 +0000

    Added custom layouts for blogs and portfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,8 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status
   ]
 
+  layout "blog"
+
   # GET /blogs
   # GET /blogs.json
   def index
