f5d5468a1686ed3463d405092c537d78edd6419b
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 17:18:33 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
+gem 'utrilla_view_tool', git: 'https://github.com/thomasutrilla/utrilla_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting" )
     end
   end
+  
+  def copyright_generator
+    UtrillaViewTool::Renderer.copyright 'Thomas Utrilla', 'All rights reserved'
+  end
 end
